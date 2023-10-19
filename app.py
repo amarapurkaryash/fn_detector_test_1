deb310ce401d8b8b42e1fc0812b13dedab1cad01
Yash Liladhar Amarapurkar <73273674+Yash19-Yash19@users.noreply.github.com>
2023-10-19 13:02:16 +0530

    1

---
+++ app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+data = pd.read_csv("news.csv")
+
+x = np.array(data["title"])
+y = np.array(data["label"])
+
+cv = CountVectorizer()
+x = cv.fit_transform(x)
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+model = MultinomialNB()
+model.fit(xtrain, ytrain)
+
+st.title("Fake News Detection System")
+
+
+def fakenewsdetection():
+    user = st.text_area("Enter Any News Headline: ")
+    if len(user) < 1:
+        st.write("  ")
+    else:
+        sample = user
+        data = cv.transform([sample]).toarray()
+        a = model.predict(data)
+        st.title(a)
+
+
+fakenewsdetection()
